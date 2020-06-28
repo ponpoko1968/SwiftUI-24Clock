0425061d86cba1298b9118b4cb5a72e44d120d25
ponpoko1968 <ponpoko1968@gmail.com>
2020-06-28 20:06:41 +0900

    Initial Commit

---
+++ TwentyFour/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TwentyFour
+//
+//  Created by 越智修司 on 2020/06/28.
+//  Copyright © 2020 ponpoko.tv. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
